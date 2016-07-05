5c2065c9ec933890f55eb2f9851e3d49440a8939
Thales Mello <thalesmello@gmail.com>
2016-07-05 14:46:49 -0300

    Include hammerspoon window switch

---
+++ hammerspoon/init.lua
@@ -0,0 +1,10 @@
+local mash = {"ctrl", "cmd"}
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
+  hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
+end)
+
+hs.hotkey.bind(mash, 'H', function() hs.window.focusedWindow():focusWindowWest() end)
+hs.hotkey.bind(mash, 'L', function() hs.window.focusedWindow():focusWindowEast() end)
+hs.hotkey.bind(mash, 'K', function() hs.window.focusedWindow():focusWindowNorth() end)
+hs.hotkey.bind(mash, 'J', function() hs.window.focusedWindow():focusWindowSouth() end)
