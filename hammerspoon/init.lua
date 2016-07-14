4151c3081e02e19d0812792c0419e6b9235a5b3b
Thales Mello <thalesmello@gmail.com>
2016-07-14 18:10:46 -0300

    Include shortcut to switch spaces

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -4,3 +4,6 @@ hs.hotkey.bind(mash, 'H', function() hs.window.focusedWindow():focusWindowWest()
 hs.hotkey.bind(mash, 'L', function() hs.window.focusedWindow():focusWindowEast() end)
 hs.hotkey.bind(mash, 'K', function() hs.window.focusedWindow():focusWindowNorth() end)
 hs.hotkey.bind(mash, 'J', function() hs.window.focusedWindow():focusWindowSouth() end)
+
+hs.hotkey.bind({"ctrl", "cmd", "shift"}, 'H', function() hs.eventtap.keyStroke({"ctrl"}, "left") end)
+hs.hotkey.bind({"ctrl", "cmd", "shift"}, 'L', function() hs.eventtap.keyStroke({"ctrl"}, "right") end)
