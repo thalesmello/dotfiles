28468a428810fa22b53fa569b08418bdda38efd2
Thales Mello <thalesmello@gmail.com>
2016-07-19 02:56:32 -0300

    Implement non recurvive maps for hammerspoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -2,12 +2,36 @@ require("hs.ipc")
 
 hs.hotkey.bind({"ctrl", "cmd", "alt"}, 'H', function() hs.toggleConsole() end)
 
-hs.hotkey.bind({"ctrl", "cmd", "shift"}, 'h', function() hs.window.focusedWindow():focusWindowWest()  end)
-hs.hotkey.bind({"ctrl", "cmd", "shift"}, 'j', function() hs.window.focusedWindow():focusWindowSouth() end)
-hs.hotkey.bind({"ctrl", "cmd", "shift"}, 'k', function() hs.window.focusedWindow():focusWindowNorth() end)
-hs.hotkey.bind({"ctrl", "cmd", "shift"}, 'L', function() hs.window.focusedWindow():focusWindowEast()  end)
+function nonRecursiveBind(mods, key, callback)
+  local hotkey
+  hotkey = hs.hotkey.bind(mods, key, function()
+    hotkey:disable()
+    callback(mods, key)
+    hotkey:enable()
+  end)
+end
+
+function except(programName, callback)
+  return function(mods, key)
+    local currentName = hs.window.focusedWindow():application():name()
+    if currentName == programName then
+      hs.eventtap.keyStroke(mods, key)
+    else
+      callback()
+    end
+  end
+end
+
+nonRecursiveBind({"ctrl"}, "H", except("iTerm2", function() hs.window.focusedWindow():focusWindowWest()  end))
+nonRecursiveBind({"ctrl"}, "J", except("iTerm2", function() hs.window.focusedWindow():focusWindowSouth() end))
+nonRecursiveBind({"ctrl"}, "K", except("iTerm2", function() hs.window.focusedWindow():focusWindowNorth() end))
+nonRecursiveBind({"ctrl"}, "L", except("iTerm2", function() hs.window.focusedWindow():focusWindowEast()  end))
+
 hs.hotkey.bind({"ctrl", "cmd"}, 'H', function() hs.eventtap.keyStroke({"ctrl"}, "left") end)
 hs.hotkey.bind({"ctrl", "cmd"}, 'L', function() hs.eventtap.keyStroke({"ctrl"}, "right") end)
+
+
+
 
 function notify(str)
   hs.notify.new({title="Hammerspoon", informativeText=str}):send()
