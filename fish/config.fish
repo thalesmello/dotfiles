8d18eb44af0da35c9e250e7388170c7079be70fd
Thales Mello <thalesmello@gmail.com>
2017-01-16 22:13:42 -0200

    Modify fzf default options

--- fish/config.fish
+++ fish/config.fish
@@ -5,4 +5,4 @@ set -gx VSUAL nvim
 # Setup envs
 status --is-interactive; and source (nodenv init -|psub)
 status --is-interactive; and source (rbenv init -|psub)
-
+set -xg FZF_DEFAULT_OPTS '--bind "ctrl-n:down,ctrl-p:up,ctrl-r:previous-history,ctrl-s:next-history,ctrl-q:select-all,ctrl-x:toggle-out"'
