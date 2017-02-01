0cb30bbdd6ac9b01eb7f56a216713168d24187ff
Thales Mello <thalesmello@gmail.com>
2017-02-01 15:53:15 -0200

    Change default theme to bobthefish

--- omf/key_bindings.fish
+++ omf/key_bindings.fish
@@ -1,11 +1,12 @@
 # Enable FZF key bindings
+fish_vi_key_bindings
 fish_default_key_bindings -M insert
+fish_vi_key_bindings --no-erase -M insert
 
 fzf_key_bindings
 # # Without an argument, fish_vi_key_bindings will default to
 # # resetting all bindings.
 # # The argument specifies the initial mode (insert, "default" or visual).
-fish_vi_key_bindings --no-erase
 # # Shift Tab
 bind -M insert \cv edit_cmd
 
