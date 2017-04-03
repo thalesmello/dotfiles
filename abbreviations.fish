83068bd2ca3d88e2634693ce6c1262b1a7c82887
Thales Mello <thalesmello@gmail.com>
2017-04-03 13:41:10 -0300

    Include abbreviations in the backup

---
+++ abbreviations.fish
@@ -0,0 +1,13 @@
+abbr gm 'git commit -m'
+abbr gs 'git status'
+abbr gd 'git diff'
+abbr ga 'git add'
+abbr gp 'git push'
+abbr vim nvim
+abbr gu 'git reset HEAD'
+abbr gco 'git checkout'
+abbr gf 'git fetch'
+abbr gcal 'gcalcli --defaultCalendar thalesmello@gmail.com --calendar Profissional'
+abbr be 'bundle exec'
+abbr how 'how2 -l'
+abbr gblame 'git log -p -M --follow --stat --'
