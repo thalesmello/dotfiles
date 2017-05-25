f61c5535d118ec3b665ca80556568292501d4fec
Thales Mello <thalesmello@gmail.com>
2017-05-25 01:11:56 -0300

    Fix setenv for nodenv and rbenv

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,6 @@ set -gx EDITOR nvim
 set -gx VISUAL nvim
 
 # Setup envs
-status --is-interactive; and source (nodenv init -|psub)
-status --is-interactive; and source (rbenv init -|psub)
+status --is-interactive; and source (nodenv init - | sed 's/setenv/set -x/g' | psub)
+status --is-interactive; and source (rbenv init -| sed 's/setenv/set -x/g' | psub)
 set -xg FZF_DEFAULT_OPTS '--bind "ctrl-n:down,ctrl-p:up,ctrl-r:previous-history,ctrl-s:next-history,ctrl-q:select-all,ctrl-x:toggle-out"'
