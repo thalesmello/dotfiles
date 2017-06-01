ec39d02c9b455992c43670d9fdb2e9565551f285
Thales Mello <thalesmello@gmail.com>
2017-06-01 15:54:53 -0300

    Improve read-password function

--- fish/functions/read-password.fish
+++ fish/functions/read-password.fish
@@ -1,7 +1,18 @@
 function read-password
-	echo -n Password:
+    set -l exports_to_variable (math (count $argv)' > 0')
+    if math $exports_to_variable >/dev/null
+        echo -n "Password: "
+    else
+        echo -n "Password: " 1>&2
+    end
+
     stty -echo
-    head -n 1 | read -gX $argv[1]
+    head -n 1 - | read -l password
     stty echo
-    echo
+
+    if math $exports_to_variable >/dev/null
+        set -xg $argv[1] $password
+    else
+        echo $password
+    end
 end
