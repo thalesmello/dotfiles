3a8395f90a4cb42d73a38da58a65825d00140a96
Thales Mello <thalesmello@gmail.com>
2017-07-07 03:21:02 -0300

    Merged keybinding configuration

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,13 @@
 function fish_user_key_bindings
+  # Enable FZF key bindings
+  fish_vi_key_bindings
+  fish_default_key_bindings -M insert
+  fish_vi_key_bindings --no-erase -M insert
+
   fzf_key_bindings
+  # # Without an argument, fish_vi_key_bindings will default to
+  # # resetting all bindings.
+  # # The argument specifies the initial mode (insert, "default" or visual).
+  # # Shift Tab
+  bind -M insert \cv edit_cmd
 end
