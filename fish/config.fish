7aa3bd23f8fe3ee279044d75af3e807dc080ec4e
Thales Mello <thalesmello@gmail.com>
2017-07-07 03:53:41 -0300

    Merge fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,20 @@
+fundle plugin brgmnn/fish-docker-compose
+fundle plugin fischerling/plugin-wd
+fundle plugin oh-my-fish/theme-bobthefish
+fundle init
+
+# Configure theme
+set -g theme_display_docker_machine no
+set -g theme_display_vi yes
+set -g theme_display_date no
+set -g theme_display_cmd_duration yes
+set -g theme_nerd_fonts yes
+set -g theme_show_exit_status yes
+set -g default_user your_normal_user
+set -g theme_color_scheme gruvbox
+set -g fish_prompt_pwd_dir_length 2
+
+thefuck --alias | source
 # Environment variables
 set -gx EDITOR nvim
 set -gx VISUAL nvim
