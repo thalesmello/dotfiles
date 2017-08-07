3033066d844dc4833330c2abcfe8506f79706b77
Thales Mello <thalesmello@gmail.com>
2017-08-07 16:03:44 -0300

    Include aws plugin

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,7 @@ fundle plugin fischerling/plugin-wd
 fundle plugin thalesmello/theme-bobthefish
 fundle plugin fisherman/rbenv
 fundle plugin fisherman/nodenv
+fundle plugin oh-my-fish/plugin-aws
 fundle init
 
 # Configure theme
