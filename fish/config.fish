382ad9a514507c6ed5b23e7b54e32d3e76d7c8dd
Thales Mello <thalesmello@gmail.com>
2018-04-05 16:38:50 -0300

    Remove virtualenv from bobthefish

--- fish/config.fish
+++ fish/config.fish
@@ -13,6 +13,7 @@ set -g theme_display_date no
 set -g theme_display_cmd_duration yes
 set -g theme_nerd_fonts yes
 set -g theme_show_exit_status yes
+set -g theme_display_virtualenv no
 set -g default_user your_normal_user
 set -g theme_color_scheme gruvbox
 set -g fish_prompt_pwd_dir_length 2
