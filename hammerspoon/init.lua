1a2b6a00a1ebadf281eeb85d21d4a2bcb2c0085f
Thales Mello <thalesmello@gmail.com>
2019-02-20 09:37:20 -0800

    Improve window management script

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -284,6 +284,11 @@ end
 hs.window.animationDuration = 0
 
 -- Resize windows
+local function approximatelyEqual(a, b)
+  local diff = math.abs(a - b)
+  return diff < 0.01
+end
+
 local function adjust(x, y, w, h)
   return function()
     local win = hs.window.focusedWindow()
@@ -298,23 +303,78 @@ local function adjust(x, y, w, h)
     f.x = math.floor((max.w * x) + max.x)
     f.y = math.floor((max.h * y) + max.y)
 
-        win:setFrame(f)
+    win:setFrame(f)
   end
 end
 
 local function adjustCenter(w, h)
   return function()
-        local win = hs.window.focusedWindow()
-        if not win then return end
+    local win = hs.window.focusedWindow()
+    if not win then return end
 
-        local f = win:frame()
-        local max = win:screen():frame()
+    local f = win:frame()
+    local max = win:screen():frame()
 
-        f.w = math.floor(max.w * w)
-        f.h = math.floor(max.h * h)
-        f.x = math.floor((max.w / 2) - (f.w / 2))
-        f.y = math.floor((max.h / 2) - (f.h / 2))
-        win:setFrame(f)
+    f.w = math.floor(max.w * w)
+    f.h = math.floor(max.h * h)
+    f.x = math.floor((max.w / 2) - (f.w / 2))
+    f.y = math.floor((max.h / 2) - (f.h / 2))
+    win:setFrame(f)
+  end
+end
+
+local function getCurrentProportions()
+  local win = hs.window.focusedWindow()
+  if not win then return end
+
+  local f = win:frame()
+  local max = win:screen():frame()
+
+  return {
+    w = f.w / max.w,
+    h = f.h / max.h,
+    x = f.x / max.w,
+    y = f.y / max.h
+  }
+end
+
+local function setWindowConfig(config)
+  local win = hs.window.focusedWindow()
+  if not win or win:isFullScreen() then return end
+
+  local f = win:frame()
+  local max = win:screen():frame()
+
+  f.w = math.floor(max.w * config.w)
+  f.h = math.floor(max.h * config.h)
+  f.x = math.floor((max.w * config.x) + max.x)
+  f.y = math.floor((max.h * config.y) + max.y)
+
+  win:setFrame(f)
+end
+
+local function adjustCycle(windowConfigs)
+  return function()
+    local win = hs.window.focusedWindow()
+    if not win or win:isFullScreen() then return end
+
+    local current = getCurrentProportions()
+
+    local config
+    local nextConfig
+
+    for i = 1, #windowConfigs do
+      config = windowConfigs[i]
+
+      if approximatelyEqual(config.w, current.w) and approximatelyEqual(config.x, current.x) then
+        nextConfig = windowConfigs[(i % #windowConfigs) + 1]
+
+        setWindowConfig(nextConfig)
+        return
+      end
+    end
+
+    setWindowConfig(windowConfigs[1])
   end
 end
 
@@ -323,7 +383,11 @@ hs.hotkey.bind(mash.ctrlCmd, "up", adjust(0, 0, 1, 0.5))
 
 -- right half
 hs.hotkey.bind(mash.ctrlCmd, "right", adjust(0.5, 0, 0.5, 1))
-hs.hotkey.bind(mash.ctrlCmd, ".", adjust(0.5, 0, 0.5, 1))
+hs.hotkey.bind(mash.ctrlCmd, ".", adjustCycle({
+  { x = 0.5, y = 0, w = 0.5, h = 1 },
+  { x = 0.75, y = 0, w = 0.25, h = 1 },
+  { x = 0.5, y = 0, w = 0.25, h = 1 }
+}))
 hs.hotkey.bind(mash.ctrlShiftCmd, ".", adjust(0.25, 0, 0.75, 1))
 
 -- bottom half
@@ -331,7 +395,11 @@ hs.hotkey.bind(mash.ctrlCmd, "down", adjust(0, 0.5, 1, 0.5))
 
 -- left half
 hs.hotkey.bind(mash.ctrlCmd, "left", adjust(0, 0, 0.5, 1))
-hs.hotkey.bind(mash.ctrlCmd, ",", adjust(0, 0, 0.5, 1))
+hs.hotkey.bind(mash.ctrlCmd, ",", adjustCycle({
+  { x = 0, y = 0, w = 0.5, h = 1 },
+  { x = 0, y = 0, w = 0.25, h = 1 },
+  { x = 0.25, y = 0, w = 0.25, h = 1 }
+}))
 hs.hotkey.bind(mash.ctrlShiftCmd, ",", adjust(0.0, 0, 0.75, 1))
 
 -- top left
