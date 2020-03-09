f096c17621c5770bd9d2857286d9d0e4d2bbd87c
Thales Mello <thalesmello@gmail.com>
2020-03-08 23:50:29 -0700

    Fix ssh config

--- fish/config.fish
+++ fish/config.fish
@@ -20,13 +20,17 @@ set -g fish_prompt_pwd_dir_length 2
 set -gx EDITOR nvim
 set -gx VISUAL nvim
 
+set -gx LIBGL_ALWAYS_INDIRECT 1
+
 # Setup envs
 set -xg FZF_DEFAULT_OPTS '--bind "ctrl-n:down,ctrl-p:up,ctrl-r:previous-history,ctrl-s:next-history,ctrl-q:select-all,ctrl-x:toggle-out"'
 set -xg SKIM_DEFAULT_OPTS '--bind "ctrl-n:down,ctrl-p:up,alt-r:previous-history,alt-s:next-history,ctrl-q:select-all,ctrl-x:toggle-out"'
 
 # AWS completion
 test -x (which aws_completer); and complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
-eval (keychain --eval --quiet)
-set -x WSL_HOST (tail -1 /etc/resolve.conf | cut -d' ' -f2)
-set -x DISPLAY=$WSL_HOST:0
+eval (keychain --eval --quiet $HOME/.ssh/id_rsa)
+set -x WSL_HOST (tail -1 /etc/resolv.conf | cut -d' ' -f2)
+set -x DISPLAY $WSL_HOST:0
+set -x HOST (hostname)
+set -x HOSTNAME (hostname)
 source $HOME/.asdf/asdf.fish
