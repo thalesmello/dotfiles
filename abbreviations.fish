c02c72ee46ce2f1c7ff85405e742b4f9ed92e1f6
Thales Mello <thalesmello@gmail.com>
2023-02-08 17:51:11 -0800

    Refactor abbreviations to new format

--- abbreviations.fish
+++ abbreviations.fish
@@ -1,21 +1,21 @@
-abbr -a -U -- be 'bundle exec'
-abbr -a -U -- dc devctl
-abbr -a -U -- dv devbox
-abbr -a -U -- ga 'git add'
-abbr -a -U -- gblame 'git log -p -M --follow --stat --'
-abbr -a -U -- gcal 'gcalcli --defaultCalendar thalesmello@gmail.com --calendar Profissional'
-abbr -a -U -- gco 'git checkout'
-abbr -a -U -- gd 'git diff'
-abbr -a -U -- gf 'git fetch'
-abbr -a -U -- gfo 'git fetch origin'
-abbr -a -U -- gm 'git commit -m'
-abbr -a -U -- gp 'git push'
-abbr -a -U -- gr 'git remote'
-abbr -a -U -- gs 'git status'
-abbr -a -U -- gu 'git reset HEAD'
-abbr -a -U -- how 'howdoi -n 5'
-abbr -a -U -- ovim /usr/local/bin/vim
-abbr -a -U -- ssx 'ssh -C -c blowfish -X'
-abbr -a -U -- vim svim
-abbr -a -U -- vp ./venv/bin/python
-abbr -a -U -- wsite 'wget -r --no-parent'
+abbr be 'bundle exec'
+abbr dc devctl
+abbr dv devbox
+abbr ga 'git add'
+abbr gblame 'git log -p -M --follow --stat --'
+abbr gcal 'gcalcli --defaultCalendar thalesmello@gmail.com --calendar Profissional'
+abbr gco 'git checkout'
+abbr gd 'git diff'
+abbr gf 'git fetch'
+abbr gfo 'git fetch origin'
+abbr gm 'git commit -m'
+abbr gp 'git push'
+abbr gr 'git remote'
+abbr gs 'git status'
+abbr gu 'git reset HEAD'
+abbr how 'howdoi -n 5'
+abbr ovim /usr/local/bin/vim
+abbr ssx 'ssh -C -c blowfish -X'
+abbr vim svim
+abbr vp ./venv/bin/python
+abbr wsite 'wget -r --no-parent'
