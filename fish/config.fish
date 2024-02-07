034a0425fd7b5985e915d42858e68742a9dc3853
Thales Mello <thalesmello@gmail.com>
2024-02-06 18:32:06 -0800

    Remove gitflow

--- fish/config.fish
+++ fish/config.fish
@@ -10,7 +10,6 @@ end
 fundle plugin brgmnn/fish-docker-compose
 fundle plugin fischerling/plugin-wd
 fundle plugin thalesmello/theme-cmorrell.com
-fundle plugin oh-my-fish/plugin-git-flow
 fundle plugin thalesmello/plugin-hubflow
 fundle plugin ankitsumitg/docker-fish-completions
 fundle init
