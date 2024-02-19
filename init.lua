7136db70ce7af7c55652b5125777863fd0a43e89
Thales Mello <thalesmello@gmail.com>
2024-02-18 18:26:15 -0800

    Add structural search replace

--- init.lua
+++ init.lua
@@ -202,6 +202,34 @@ require("lazy").setup({
             -- highlight = { "Function", "Label" },
             priority = 500,
         }
-    } }
+    } },
+    {
+
+        "cshuaimin/ssr.nvim",
+        module = "ssr",
+        -- Calling setup is optional.
+        config = function()
+            require("ssr").setup {
+                border = "rounded",
+                min_width = 50,
+                min_height = 5,
+                max_width = 120,
+                max_height = 25,
+                adjust_window = true,
+                keymaps = {
+                    close = "q",
+                    next_match = "n",
+                    prev_match = "N",
+                    replace_confirm = "<cr>",
+                    replace_all = "<leader><cr>",
+                },
+            }
+
+            vim.keymap.set("n", "<leader>sr", function()
+                require("ssr").open()
+            end, { desc = "[S]tructural [R]eplace" })
+        end
+
+    }
 })
 
