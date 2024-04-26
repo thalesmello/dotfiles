07640022465c85375ade3a948da62f9b6a01d274
Thales Mello <thalesmello@gmail.com>
2024-04-26 15:52:00 -0700

    Improve aws sso function

--- fish/functions/aws-sso-login.fish
+++ fish/functions/aws-sso-login.fish
@@ -1,7 +1,10 @@
 function aws-sso-login --argument profile
     true
     and set -Ux AWS_PROFILE $profile
-    and aws sso login
+
+    if not aws --profile "$profile" sts get-caller-identity >/dev/null 2>&1
+        aws sso login
+    end
 end
 
 complete -c aws-sso-login -n '__fish_is_first_token' -xa '(complete -C "aws sso login --profile ")'
