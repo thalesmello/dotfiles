60bfd29f205d2007e1626a046388b14aeafbb177
Thales Mello <thalesmello@gmail.com>
2024-05-08 20:25:28 -0700

    Extract ag completion to its own file

--- fish/functions/__ag_completion_smart.fish
+++
@@ -1,6 +0,0 @@
-function __ag_completion_smart
-    set -l tokens (commandline --current-process --tokenize | string match -re '^[^-]')
-    set -g query '\b\w*'"$(string escape $tokens[2])"'\w*\b'
-    set -g results (ag -o "$query" | cut -d":" -f3- | sort | uniq -c | sort -k1,1 -n -r | awk '{ print $2 }')
-    printf '%s\n' $results
-end
--- fish/functions/__ag_is_positional_arg.fish
+++
@@ -1,8 +0,0 @@
-function __ag_is_positional_arg --argument pos
-    set -l curr_tokens (commandline --cut-at-cursor --current-process --tokenize | string match -re '^[^-]')
-    if test (count $curr_tokens) -eq $pos
-        return 0
-    else
-        return 1
-    end
-end
