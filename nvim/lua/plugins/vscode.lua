30990c003c7cce013538e214d03b7847cf84c1b8
Thales Mello <thalesmello@meta.com>
2024-10-31 03:04:14 -0700

    Tweak vscode

--- nvim/lua/plugins/vscode.lua
+++ nvim/lua/plugins/vscode.lua
@@ -16,6 +16,6 @@ end
 -- vim.keymap.set("n", "<leader><space>", "<cmd>Find<cr>")
 vim.keymap.set("n", "<leader>/", [[<cmd>lua require('vscode').action('workbench.action.findInFiles')<cr>]])
 vim.keymap.set("n", "<leader>ss", [[<cmd>lua require('vscode').action('workbench.action.gotoSymbol')<cr>]])
-vim.keymap.set("n", "<leader><space>", [[<cmd>lua require('vscode').action('workbench.action.terminal.toggleTerminal')<cr>]])
+vim.keymap.set("n", "<leader><cr>", [[<cmd>lua require('vscode').action('workbench.action.terminal.toggleTerminal')<cr>]])
 
 return {}
