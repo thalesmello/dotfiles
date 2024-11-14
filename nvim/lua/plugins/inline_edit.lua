bb7e579aed493a0de2b8a057c6bf72a1659ff36e
Thales Mello <thalesmello@meta.com>
2024-11-13 21:43:54 -0800

    Update

--- nvim/lua/plugins/inline_edit.lua
+++ nvim/lua/plugins/inline_edit.lua
@@ -2,7 +2,8 @@ return {
     'AndrewRadev/inline_edit.vim',
     keys = {
         {'<leader>ie',  '<cmd>InlineEdit<cr>',  silent = true},
-        {'<leader>ie', ':InlineEdit<space>""<left>', remap = true, mode = "x"}
+        -- We navigate up to fetch the last used InlineEdit used command
+        {'<leader>ie', ':InlineEdit<space><up>', remap = true, mode = "x"}
     },
     cmd = "InlineEdit",
     firenvim = true,
