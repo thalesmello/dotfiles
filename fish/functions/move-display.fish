c832ba21109e897b24e390bea8de0c9e0e029276
Thales Mello <thalesmello@meta.com>
2024-11-27 12:26:55 -0800

    Improve move-display args and completion

--- fish/functions/move-display.fish
+++ fish/functions/move-display.fish
@@ -1,6 +1,25 @@
 function move-display -a location
-    betterdisplaycli perform --originalName (osascript -e 'tell application "System Events" to display name of desktop 2') --targetOriginalName (osascript -e 'tell application "System Events" to display name of desktop 1') --moveTo $location
+    argparse 'main=' -- $argv
+
+    if set -q _flag_main
+        betterdisplaycli set '--main=on' "--originalName=$_flag_main"
+    end
+
+    set location "$argv[1]"
+    set mainDisplay (osascript -e 'tell application "System Events" to display name of desktop 1')
+    set secondaryDisplay (osascript -e 'tell application "System Events" to display name of desktop 2')
+
+    betterdisplaycli perform --originalName $secondaryDisplay --targetOriginalName $mainDisplay --moveTo "$location"
+end
+
+function __move_display_nth_token
+    set n "$argv[1]"
+    set -l tokens (commandline -poc)
+    argparse 'main=' -- $tokens
+
+    test (count $argv) -eq "$n"
 end
 
 complete -c move-display -f
-complete -c move-display -n "__fish_is_nth_token 1" -f -d "Location of the secondary display in relation to main display" -a "topLeftCorner topLeft top topRight topRightCorner leftTop left leftBottom rightTop right rightBottom bottomLeftCorner bottomLeft bottom bottomRight bottomRightCorner"
+complete -c move-display -n "__move_display_nth_token 1" -f -d "Location of the secondary display in relation to main display" -a "topLeftCorner topLeft top topRight topRightCorner leftTop left leftBottom rightTop right rightBottom bottomLeftCorner bottomLeft bottom bottomRight bottomRightCorner"
+complete -c move-display -x -l "main" -d "Name of the display to make main" -a "(complete -C 'betterdisplaycli set --main=on --originalName ')"
