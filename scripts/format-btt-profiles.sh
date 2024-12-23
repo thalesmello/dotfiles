8118084097c5cde539c18a9c2d24efe0b9f2350b
Thales Mello <thalesmello@meta.com>
2024-12-23 14:41:06 -0800

    Simplify format command

--- scripts/format-btt-profiles.sh
+++ scripts/format-btt-profiles.sh
@@ -5,12 +5,7 @@ set -e
 DIR="$(dirname $(dirname $(realpath $0)))"
 
 for filename in "$DIR"/bettertouchtool/*.bttpreset; do
-    # echo -- "-e" "$filename"
-    # [ -e "$filename" ] || echo ""
-
     echo "Running custom command..."
     echo yq -P "'"'(.. | sort_keys(.) | select(tag == "!!seq")) |= sort_by(.BTTOrder // 1000)'"'" -o json -i "'$filename'"
     yq -P '(.. | sort_keys(.) | select(tag == "!!seq")) |= sort_by(.BTTOrder // 1000)' -o json -i "$filename"
-
 done
-# Add your custom command logic here
