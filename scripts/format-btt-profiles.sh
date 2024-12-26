1bb6aaaf4819d2927efa2dd018830410a4e50957
Thales Mello <thalesmello@meta.com>
2024-12-26 11:47:45 -0800

    Update syntax to format btt profile

--- scripts/format-btt-profiles.sh
+++ scripts/format-btt-profiles.sh
@@ -6,6 +6,6 @@ DIR="$(dirname $(dirname $(realpath $0)))"
 
 for filename in "$DIR"/bettertouchtool/*.bttpreset; do
     echo "Running custom command..."
-    echo yq -P "'"'(.. | sort_keys(.) | select(tag == "!!seq")) |= sort_by(.BTTOrder // 1000)'"'" -o json -i "'$filename'"
-    yq -P '(.. | sort_keys(.) | select(tag == "!!seq")) |= sort_by(.BTTOrder // 1000)' -o json -i "$filename"
+    echo yq -P "'"'(.. | sort_keys(.) | select(tag == "!!seq")) |= sort_by(.BTTOrder // 1000) | .BTTPresetContent |= sort_by(.BTTAppBundleIdentifier)'"'" -o json -i "'$filename'"
+    yq -P '(.. | sort_keys(.) | select(tag == "!!seq")) |= sort_by(.BTTOrder // 1000) | .BTTPresetContent |= sort_by(.BTTAppBundleIdentifier)' -o json -i "$filename"
 done
