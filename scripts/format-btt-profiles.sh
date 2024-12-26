76cc4f122c6b0e5c529af98c7ed1eaa02126eac6
Thales Mello <thalesmello@meta.com>
2024-12-26 14:52:30 -0800

    Update format of btt

--- scripts/format-btt-profiles.sh
+++ scripts/format-btt-profiles.sh
@@ -6,6 +6,6 @@ DIR="$(dirname $(dirname $(realpath $0)))"
 
 for filename in "$DIR"/bettertouchtool/*.bttpreset; do
     echo "Running custom command..."
-    echo yq -P "'"'.BTTPresetContent |= sort_by(.BTTAppBundleIdentifier) | (.. | select(kind == "!!seq")) |= sort_by(.BTTOrder // 1000) | .BTTPresetContent[].BTTTriggers |= sort_by(.BTTTriggerClass) | sort_keys(..)'"'" -o json -i "'$filename'"
-    yq -P '.BTTPresetContent |= sort_by(.BTTAppBundleIdentifier) | (.. | select(kind == "!!seq")) |= sort_by(.BTTOrder // 1000) | .BTTPresetContent[].BTTTriggers |= sort_by(.BTTTriggerClass) | sort_keys(..)' -o json -i "$filename"
+    echo yq -P "'"'(.. | select(tag == "!!seq")) |= sort_by(.BTTAppBundleIdentifier, .BTTTriggerParentUUID, .BTTTriggerClass, .BTTOrder) | sort_keys(..)'"'" -o json -i "'$filename'"
+    yq -P '(.. | select(tag == "!!seq")) |= sort_by(.BTTAppBundleIdentifier, .BTTTriggerParentUUID, .BTTTriggerClass, .BTTOrder) | sort_keys(..)' -o json -i "$filename"
 done
