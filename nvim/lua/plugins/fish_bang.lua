92e39dfa6e9c30dee802c1880b084ee14c857561
Thales Mello <thalesmello@gmail.com>
2025-01-15 23:06:20 -0800

    Update fishbang

--- nvim/lua/plugins/fish_bang.lua
+++ nvim/lua/plugins/fish_bang.lua
@@ -1,9 +1,11 @@
 return {
    {
-        dir = vim.fn.stdpath("config") .. "/local_plugins/fish-bang/",
-        dev = true,
-        config = function ()
-            require('fish_bang').setup()
-        end
-    }
+      dir = vim.fn.stdpath("config") .. "/local_plugins/fish-bang/",
+      dev = true,
+      config = function ()
+         require('fish_bang').setup()
+
+         vim.keymap.set("x", "!", ":Fish<space>", { remap = false })
+      end
+   }
 }
