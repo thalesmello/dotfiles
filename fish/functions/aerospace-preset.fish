b3256930bea332afad6890ad033d013bcca0cc30
Thales Mello <thalesmello@meta.com>
2025-02-13 15:26:46 -0800

    aerospace: Add preset to minimize windows

--- fish/functions/aerospace-preset.fish
+++ fish/functions/aerospace-preset.fish
@@ -7,6 +7,9 @@ function aerospace-preset
 
     set current_workspace (aerospace list-workspaces --focused --format "%{workspace}")
     set back_workspace "$_flag_back_workspace"
+    set current_window (aerospace list-windows --focused --format '%{window-id}')
+    set windows (aerospace list-windows --workspace $current_workspace --format "%{window-id}")
+    set other_windows (string match -v -e "$current_window" $windows)
 
     if test -z "$back_workspace"
         set back_workspace 2
@@ -17,16 +20,22 @@ function aerospace-preset
     end
 
     if test "$preset" = "move-other-windows"
-        set current_window (aerospace list-windows --focused --format '%{window-id}')
-        set windows (aerospace list-windows --workspace $current_workspace --format "%{window-id}")
-
-        for window in $windows
-            test "$window" = "$current_window"; and continue
+        for window in $other_windows
             aerospace move-node-to-workspace --window-id $window $back_workspace
+        end
+    else if test "$preset" = "minimize-other-windows"
+        for window in $other_windows
+            aerospace focus --window-id $window
+            aerospace macos-native-minimize
+        end
+    else if test "$preset" = "minimize-windows"
+        for window in $windows
+            aerospace focus --window-id $window
+            aerospace macos-native-minimize
         end
     end
 end
 
 complete -c aerospace-preset -f
-complete -c aerospace-preset -n "__fish_is_nth_token 1" -f -d "Name of the preset to use" -a "move-other-windows"
+complete -c aerospace-preset -n "__fish_is_nth_token 1" -f -d "Name of the preset to use" -a "move-other-windows minimize-windows minimize-other-windows"
 complete -c aerospace-preset -n "test (count (commandline -opc)) -gt 1" -x -l "back-workspace" -d "Workspace to send windows to" -a "(aerospace list-workspaces --all --format '%{workspace}%{tab}%{monitor-name}')"
