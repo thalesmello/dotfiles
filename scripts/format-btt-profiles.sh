f1a72fd5b862f215e12fe6aef859b6191c1a9550
Thales Mello <thalesmello@gmail.com>
2025-03-09 22:43:33 -0700

    btt: Update workflow and fix format script

--- scripts/format-btt-profiles.sh
+++ scripts/format-btt-profiles.sh
@@ -6,6 +6,10 @@ DIR="$(dirname $(dirname $(realpath $0)))"
 
 for filename in "$DIR"/bettertouchtool/*.bttpreset; do
     echo "Running custom command..."
+    echo sed -i.bak 's!\\/!/!g' "'$filename'"
+    sed -i.bak 's!\\/!/!g' "$filename"
     echo yq -P "'"'(.. | select(tag == "!!seq")) |= sort_by(.BTTAppBundleIdentifier, .BTTTriggerParentUUID, .BTTTriggerClass, .BTTOrder) | sort_keys(..)'"'" -o json -i "'$filename'"
     yq -P '(.. | select(tag == "!!seq")) |= sort_by(.BTTAppBundleIdentifier, .BTTTriggerParentUUID, .BTTTriggerClass, .BTTOrder) | sort_keys(..)' -o json -i "$filename"
+    echo rm "'$filename.bak'"
+    rm "$filename.bak"
 done
