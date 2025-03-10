239dc9a17720295944441459f5c550a30d4d7369
Thales Mello <thalesmello@gmail.com>
2025-03-09 18:24:43 -0700

    macos: disable tap to click and hold for accent in defaults

--- scripts/macos_settings.sh
+++ scripts/macos_settings.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 # key repeat
-defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
-defaults write -g KeyRepeat -int
+defaults write -g InitialKeyRepeat -int 15 # normal minimum is 15 (225 ms)
+defaults write -g KeyRepeat -int 2
 
-# three fingers drag
+# enable three fingers drag
 defaults -currentHost write NSGlobalDomain com.apple.trackpad.threeFingerSwipeGesture -int 1
 
+# disable hold key for accent mac os
+defaults write -g ApplePressAndHoldEnabled -bool true
 
+# disable tap to click
+defaults write "com.apple.AppleMultitouchTrackpad" "Clicking" -bool false
