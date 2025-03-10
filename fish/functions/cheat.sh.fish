415fde3595203704a038f9cc3e2a93397a28cea1
Thales Mello <thalesmello@gmail.com>
2025-03-09 20:30:39 -0700

    fish: Add cheat.sh function

---
+++ fish/functions/cheat.sh.fish
@@ -0,0 +1,6 @@
+function cheat.sh
+    curl cheat.sh/$argv
+end
+
+# register completions (on-the-fly, non-cached, because the actual command won't be cached anyway
+complete -c cheat.sh -xa '(curl -s cheat.sh/:list)'
