dddcc1b04f54a620dd3abb32b136fa0a77c18a18
Thales Mello <thalesmello@meta.com>
2025-04-09 12:46:29 -0700

    aerospace: temporarily change auto-summon behavior

--- fish/functions/aerospace-on-workspace-change.fish
+++ fish/functions/aerospace-on-workspace-change.fish
@@ -1,6 +1,7 @@
 function aerospace-on-workspace-change
-    aerospace-auto-summon \
-        --when-workspace "?"
+    # Currently too slow. I'm disabling it to check whether it improves performance
+    # aerospace-auto-summon \
+    #     --when-workspace "?"
     aerospace-sticky-windows \
         --except-workspace "?" \
         -w "Picture in Picture" \
