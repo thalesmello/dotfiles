291349240b7778654fbb5358aa895eace40a8cc8
Thales Mello <thalesmello@gmail.com>
2025-04-28 01:16:29 -0700

    yabai-preset: Fix bugs in focus window

--- fish/functions/yabai-preset.fish
+++ fish/functions/yabai-preset.fish
@@ -6,9 +6,14 @@ function yabai-preset
         set direction $argv[1]
         set -e argv[1]
         set winid (yabai -m query --windows | jq -e --arg direction "$direction" '. | map(.frame |= . + {x: (.x/2 + .w/2), y: (.y/2 + .h/2)}) | (.[] | select(."has-focus")) as {$id, $app, frame: $zero}
-        | map(select(."is-visible") | .zero = $zero)
+        | map(select(."is-visible" and (."is-sticky"|not)) | .zero = $zero)
         | map(.frame |= . + {distance: (pow(.x - $zero.x; 2) + pow(.y - $zero.y; 2)), angle: atan2(-.y + $zero.y; .x - $zero.x)})
-        | {east: {lt: (3.14/4), gte: (-3.14/4)}, north: {lt: (3*3.14/4), gte: (3.14/4)}, west: {lt: (-3*3.14/4), gte: (3*3.14/4)}, south: {gte: (-3*3.14/4), lt: (-3.14/4)}}[$direction] as $filter
+        | {
+        east: {lt: (3.14/4), gte: (-3.14/4)},
+        north: {lt: (3*3.14/4), gte: (3.14/4)},
+        west: {lt: (5*3.14/4), gte: (3*3.14/4), shiftnegative: true},
+        south: {gte: (-3*3.14/4), lt: (-3.14/4)}}[$direction] as $filter
+        | map(.frame.angle |= if $filter.shiftnegative and . < 0 then (. + 2*3.14) else . end)
         | map(select(.frame.angle >= $filter.gte and .frame.angle < $filter.lt and .id != $id))
         | sort_by(.frame.distance)
         | first
