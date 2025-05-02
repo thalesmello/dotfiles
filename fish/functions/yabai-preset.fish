f4d580c388a341e2add642326bbac3f6c824e8b9
Thales Mello <thalesmello@meta.com>
2025-05-02 14:45:56 -0700

    yabai: Add btt display HUD

--- fish/functions/yabai-preset.fish
+++ fish/functions/yabai-preset.fish
@@ -101,34 +101,27 @@ function yabai-preset
         set space $argv[1]
         set -e argv[1]
 
-        if test "$space" = next
-            set json '{"BTTPredefinedActionType":114}'
-        else if test "$space" = prev
-            set json '{"BTTPredefinedActionType":113}'
-        else if test "$space" = recent
-            set spc (yabai -m query --spaces --space recent | jq .index)
-            set json (jq -nc --argjson spc "$spc" '{"BTTPredefinedActionType":(206 + $spc)}')
-        else
-            set json (jq -nc --argjson spc "$space" '{"BTTPredefinedActionType":(206 + $spc)}')
+        if contains "$space" "next" "prev" "recent"
+            set space (yabai -m query --spaces --space "$space" | jq .index)
         end
 
-        curl "$btt_url" --url-query "json=$json"
+        set json (jq -nc --argjson spc "$space" '{"BTTPredefinedActionType":(206 + $spc)}')
+        set json (string escape --style=url "$json")
+        curl -G "$btt_url" -d "json=$json"
+        yabai-preset display-message "Space $space"
     else if test "$preset" = "move-window-to-space"
         set space $argv[1]
         set -e argv[1]
 
-        if test "$space" = next
-            set json '{"BTTPredefinedActionType":152}'
-        else if test "$space" = prev
-            set json '{"BTTPredefinedActionType":151}'
-        else if test "$space" = recent
-            set spc (yabai -m query --spaces --space recent | jq .index)
-            set json (jq -nc --argjson spc "$spc" '{"BTTPredefinedActionType":(215 + $spc)}')
-        else
-            set json (jq -nc --argjson spc "$space" '{"BTTPredefinedActionType":(215 + $spc)}')
+        if contains "$space" "next" "prev" "recent"
+            set space (yabai -m query --spaces --space "$space" | jq .index)
         end
 
-        curl "$btt_url" --url-query "json=$json"
+        set json (jq -nc --argjson spc "$space" '{"BTTPredefinedActionType":(215 + $spc)}')
+        set json (string escape --style=url "$json")
+
+        curl -G "$btt_url" -d "json=$json"
+        yabai-preset display-message "Space $space"
     else if test "$preset" = "focus-display-with-fallback"
         set display $argv[1]
         set -e argv[1]
@@ -147,7 +140,8 @@ function yabai-preset
         set win (yabai -m query --windows --window | jq '.id')
         set fallback next
         set json '{"BTTPredefinedActionType":47}'
-        curl "$btt_url" --url-query "json=$json"
+        set json (string escape --style=url "$json")
+        curl -G "$btt_url" -d "json=$json"
         yabai -m window "$win" --focus
     else if test "$preset" = "arrange-windows-side-by-side"
         yabai -m query --windows --space | jq 'map(select(."is-visible" and (."is-sticky"|not))) | "\(first(.[] | select(."has-focus") | .id)):\(first(.[] | select(."has-focus"|not) | .id))"'
@@ -164,6 +158,18 @@ function yabai-preset
         else
             yabai -m window --stack "$direction"
         end
+    else if test "$preset" = "display-message"
+        set message $argv[1]
+        set -e argv[1]
+
+        set json (jq -n --arg message "$message" '{
+    "BTTPredefinedActionType" : 254,
+    "BTTHUDActionConfiguration" : "{\\"BTTActionHUDBlur\\":true,\\"BTTActionHUDBackground\\":\\"0.000000, 0.000000, 0.000000, 0.000000\\",\\"BTTIconConfigImageHeight\\":100,\\"BTTActionHUDPosition\\":0,\\"BTTActionHUDDetail\\":\\"\\",\\"BTTActionHUDDuration\\":0.15000000596046448,\\"BTTActionHUDDisplayToUse\\":0,\\"BTTIconConfigImageWidth\\":100,\\"BTTActionHUDSlideDirection\\":0,\\"BTTActionHUDHideWhenOtherHUDAppears\\":false,\\"BTTActionHUDWidth\\":220,\\"BTTActionHUDAttributedTitle\\":\\"{\\\\\\\\rtf1\\\\\\\\ansi\\\\\\\\ansicpg1252\\\\\\\\cocoartf2822\\\\n\\\\\\\\cocoatextscaling0\\\\\\\\cocoaplatform0{\\\\\\\\fonttbl\\\\\\\\f0\\\\\\\\fswiss\\\\\\\\fcharset0 Helvetica-Bold;}\\\\n{\\\\\\\\colortbl;\\\\\\\\red255\\\\\\\\green255\\\\\\\\blue255;\\\\\\\\red0\\\\\\\\green0\\\\\\\\blue0;}\\\\n{\\\\\\\\*\\\\\\\\expandedcolortbl;;\\\\\\\\cssrgb\\\\\\\\c0\\\\\\\\c0\\\\\\\\c0\\\\\\\\c84706\\\\\\\\cname labelColor;}\\\\n\\\\\\\\pard\\\\\\\\tx560\\\\\\\\tx1120\\\\\\\\tx1680\\\\\\\\tx2240\\\\\\\\tx2800\\\\\\\\tx3360\\\\\\\\tx3920\\\\\\\\tx4480\\\\\\\\tx5040\\\\\\\\tx5600\\\\\\\\tx6160\\\\\\\\tx6720\\\\\\\\pardirnatural\\\\\\\\qc\\\\\\\\partightenfactor0\\\\n\\\\n\\\\\\\\f0\\\\\\\\b\\\\\\\\fs80 \\\\\\\\cf2 \($message)}\\",\\"BTTActionHUDBorderWidth\\":0,\\"BTTActionHUDTitle\\":\\"\\",\\"BTTIconConfigIconType\\":2,\\"BTTActionHUDHeight\\":220}",
+  }')
+
+        set json (string escape --style=url "$json")
+        curl -G "$btt_url" -d "json=$json"
+
     else if false
     end
 
