7bec25ae7abf7b7512d473b89df2d58a4241c9da
Thales Mello <thalesmello@meta.com>
2025-05-07 01:58:18 -0700

    btt-preset: Add helper functions

---
+++ fish/functions/btt-preset.fish
@@ -0,0 +1,68 @@
+function btt-preset
+    set preset $argv[1]
+    set -e argv[1]
+    set btt_url 'http://localhost:12000/trigger_action/'
+
+    if test "$preset" = "trigger-action"
+        set json $argv[1]
+        set -e argv[1]
+
+        set json (string escape --style=url "$json")
+        curl -G "$btt_url" -d "json=$json"
+    else if test "$preset" = "focus-space"
+        set space $argv[1]
+        set -e argv[1]
+
+        set json (jq -nc --argjson spc "$space" '{"BTTPredefinedActionType":(206 + $spc)}')
+
+        btt-preset trigger-action "$json"
+    else if test "$preset" = "move-window-to-space"
+        set space $argv[1]
+        set -e argv[1]
+
+        set json (jq -nc --argjson spc "$space" '{"BTTPredefinedActionType":(215 + $spc + if $spc >= 6 then 1 else 0 end)}')
+
+        btt-preset trigger-action "$json"
+    else if test "$preset" = "move-window-to-next-display"
+        set json (jq -nc --argjson spc "$space" '{"BTTPredefinedActionType":(206 + $spc)}')
+        btt-preset trigger-action "$json"
+    else if test "$preset" = "display-message"
+        set message $argv[1]
+        set -e argv[1]
+
+        set json "$(jq -n --arg message "$message" '{
+          "BTTPredefinedActionType": 254,
+          "BTTHUDActionConfiguration": {
+            "BTTActionHUDBlur": true,
+            "BTTActionHUDBackground": "0.000000, 0.000000, 0.000000, 0.000000",
+            "BTTIconConfigImageHeight": 100,
+            "BTTActionHUDPosition": 0,
+            "BTTActionHUDDetail": "",
+            "BTTActionHUDDuration": 0.15000000596046448,
+            "BTTActionHUDDisplayToUse": 0,
+            "BTTIconConfigImageWidth": 100,
+            "BTTActionHUDSlideDirection": 0,
+            "BTTActionHUDHideWhenOtherHUDAppears": false,
+            "BTTActionHUDWidth": 220,
+            "BTTActionHUDAttributedTitle": "{\\\\rtf1\\\\ansi\\\\ansicpg1252\\\\cocoartf2822\\n\\\\cocoatextscaling0\\\\cocoaplatform0{\\\\fonttbl\\\\f0\\\\fswiss\\\\fcharset0 Helvetica-Bold;}\\n{\\\\colortbl;\\\\red255\\\\green255\\\\blue255;\\\\red0\\\\green0\\\\blue0;}\\n{\\\\*\\\\expandedcolortbl;;\\\\cssrgb\\\\c0\\\\c0\\\\c0\\\\c84706\\\\cname labelColor;}\\n\\\\pard\\\\tx560\\\\tx1120\\\\tx1680\\\\tx2240\\\\tx2800\\\\tx3360\\\\tx3920\\\\tx4480\\\\tx5040\\\\tx5600\\\\tx6160\\\\tx6720\\\\pardirnatural\\\\qc\\\\partightenfactor0\\n\\n\\\\f0\\\\b\\\\fs80 \\\\cf2 \\($message)}",
+            "BTTActionHUDBorderWidth": 0,
+            "BTTActionHUDTitle": "",
+            "BTTIconConfigIconType": 2,
+            "BTTActionHUDHeight": 220
+          }
+        }')"
+
+        echo "$json"
+        btt-preset trigger-action "$json"
+    else if test "$preset" = "trigger-menu-bar"
+        set path $argv[1]
+        set -e argv[1]
+
+        set json "$(jq -n --arg path "$path" '{
+            "BTTPredefinedActionType" : 124,
+            "BTTMenubarPath" : $path,
+        }')"
+
+        btt-preset trigger-action "$json"
+    end
+end
