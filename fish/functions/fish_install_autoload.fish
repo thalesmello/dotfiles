e1d335a1900f495620c512418ceeca95e4cd44e3
Thales Mello <thalesmello@meta.com>
2025-05-11 01:57:31 -0700

    fish: Fix bug in autoload

--- fish/functions/fish_install_autoload.fish
+++ fish/functions/fish_install_autoload.fish
@@ -2,10 +2,12 @@ function fish_install_autoload
     # We list possible brew locations, in order of precedence
     set brews '/opt/homebrew/bin/original-brew' '/opt/homebrew/bin/brew' '/home/linuxbrew/.linuxbrew/bin/brew'
 
-    for brew in $brews
-        test -x "$brew"
-        and "$brew" shellenv > "$__fish_data_dir/vendor_conf.d/brew.fish"
-        and break
+    if not test -e "$__fish_data_dir/vendor_conf.d/brew.fish"
+        for brew in $brews
+            test -x "$brew"
+            and "$brew" shellenv > "$__fish_data_dir/vendor_conf.d/brew.fish"
+            and break
+        end
     end
 
     fzf --fish > "$__fish_data_dir/vendor_conf.d/fzf.fish"
