97d0dca5159b38dc7c3da4b6f2bee66b28d3f8bd
Thales Mello <thalesmello@meta.com>
2025-05-12 01:15:34 -0700

    nvim: Restore lastplace

--- nvim/lua/plugins/vim_lastplace.lua
+++ nvim/lua/plugins/vim_lastplace.lua
@@ -1,15 +1,6 @@
 return {
     {
         'farmergreg/vim-lastplace',
-        cond = false,
         event = { "BufReadPost", "BufNewFile", "BufFilePost" },
-    },
-    {
-        'ethanholz/nvim-lastplace',
-        opt = {
-            lastplace_ignore_buftype = {"quickfix", "nofile", "help"},
-            lastplace_ignore_filetype = {"gitcommit", "gitrebase", "svn", "hgcommit"},
-            lastplace_open_folds = true,
-        },
     }
 }
