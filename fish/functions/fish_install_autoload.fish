8b76c8a63cd31f7a345093bc7bf936e832a127e4
Thales Mello <thalesmello@meta.com>
2025-05-13 14:53:31 -0700

    yabai: Temporarily disable restore floating windows

--- fish/functions/fish_install_autoload.fish
+++ fish/functions/fish_install_autoload.fish
@@ -1,14 +1,17 @@
 function fish_install_autoload
+    mkdir -p "$__fish_user_data_dir/vendor_conf.d/"
+
     # We list possible brew locations, in order of precedence
     set brews '/opt/homebrew/bin/original-brew' '/opt/homebrew/bin/brew' '/home/linuxbrew/.linuxbrew/bin/brew'
 
-    if not test -e "$__fish_data_dir/vendor_conf.d/brew.fish"
+    if not test -e "$__fish_user_data_dir/vendor_conf.d/brew.fish"
         for brew in $brews
             test -x "$brew"
-            and "$brew" shellenv > "$__fish_data_dir/vendor_conf.d/brew.fish"
+            and "$brew" shellenv > "$__fish_user_data_dir/vendor_conf.d/brew.fish"
+            and source "$__fish_user_data_dir/vendor_conf.d/brew.fish"
             and break
         end
     end
 
-    fzf --fish > "$__fish_data_dir/vendor_conf.d/fzf.fish"
+    fzf --fish > "$__fish_user_data_dir/vendor_conf.d/fzf.fish"
 end
