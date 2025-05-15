f77eec9ecedcc2edbfe4b70565b20027ddc665d8
Thales Mello <thalesmello@meta.com>
2025-05-15 05:17:47 -0700

    skhd: Update shortcuts

--- nvim/lua/ftmini/sql.lua
+++ nvim/lua/ftmini/sql.lua
@@ -4,7 +4,7 @@ local spec_treesitter = require('mini.ai').gen_spec.treesitter
 return {
     custom_textobjects = {
         [","] = spec_treesitter({ i = "@sql-term-expr", a = "@sql-term-term" }),
-        ["S"] = { {"^[%w_].*", "[^%w_].*"}, "^[^%w_]?[%w_]*%s+[Aa][Ss]%s+%b(),?", "^.()[%w_]-%s+..%s+%(%s*().-()%s*%),?()$"},
+        ["S"] = { {"^[%w_].*", "[^%w_].*"}, "^[^%w_]-[%w_]+%s+[Aa][Ss]%s+%b(),?", "^.()[%w_]-%s+..%s+%(%s*().-()%s*%),?()$"},
         ["s"] = spec_treesitter({ i = "@sql-select-inner", a = "@sql-select-statement" }),
         ['i'] = spec_pair('{{', '}}'),
     },
