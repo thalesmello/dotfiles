a1f2b07a93fa5219a575907b3e04ee66799b3fe6
Thales Mello <thalesmello@meta.com>
2025-05-20 02:51:41 +0100

    nvim: Add messages map

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -7,6 +7,7 @@ vim.keymap.set("n", "]<Tab>", "<cmd>tabnext<cr>", { remap = true })
 vim.keymap.set("n", "<leader>ww", "<cmd>w<cr>", { remap = true })
 vim.keymap.set({"n", "x", "o"}, "H", "^", { remap = false })
 vim.keymap.set({"n", "x", "o"}, "L", "$", { remap = false })
+vim.keymap.set({"n", "x", "o"}, "<leader>me", "<cmd>messages<cr>", { remap = false })
 
 local lineContinuationToggle = [=[:luado if line:match('\\?%s*#.*$') ~= nil then return line:gsub('^(%s*)\\?.-# ', '%1') elseif vim.fn.getline('.'):match('\\$') then return line:gsub('^(%s*)(%S-.-)$', '%1\\ # %2') else return line:gsub('^(%s*)(%S-.-)$', '%1# %2') end<cr>]=]
 
