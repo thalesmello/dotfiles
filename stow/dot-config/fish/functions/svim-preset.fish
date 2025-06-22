e008eeb0077307d3377b76ace6e58c7538ac9da1
Thales Mello <thalesmello@gmail.com>
2025-06-23 01:19:18 +0200

    skhd: Add svim mode

--- stow/dot-config/fish/functions/svim-preset.fish
+++ stow/dot-config/fish/functions/svim-preset.fish
@@ -1,13 +1,16 @@
 function svim-preset
     set mode $argv[1]
     set -e argv[1]
-    
+
     switch "$mode"
     case "enter"
-        echo hi
+        svim > /tmp/svim.log &
+        skhd -k "cmd - tab"
+        skhd -k "cmd - tab"
     case "exit"
-        echo bye
+        killall svim
     end
 end
 
 complete -c svim-preset -f
+complete -c svim-preset -n "__fish_is_nth_token 1" -a "enter exit"
