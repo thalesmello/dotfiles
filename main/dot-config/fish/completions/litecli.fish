4656a566a769741a2340a61d23be81e4c29a9f91
Thales Mello <thalesmello@gmail.com>
2025-07-10 13:52:39 +0200

    fish: Add litecli completion for fish

---
+++ main/dot-config/fish/completions/litecli.fish
@@ -0,0 +1,18 @@
+function _litecli_completion;
+    set -l response (env _LITECLI_COMPLETE=fish_complete COMP_WORDS=(commandline -cp) COMP_CWORD=(commandline -t) litecli);
+
+    for completion in $response;
+        set -l metadata (string split "," $completion);
+
+        if test $metadata[1] = "dir";
+            __fish_complete_directories $metadata[2];
+        else if test $metadata[1] = "file";
+            __fish_complete_path $metadata[2];
+        else if test $metadata[1] = "plain";
+            echo $metadata[2];
+        end;
+    end;
+end;
+
+complete --command litecli --arguments "(_litecli_completion)";
+
