cd2520a4dbd3cdeab8e14186b671deea88b9bfdc
Thales Mello <thalesmello@meta.com>
2025-07-25 13:20:20 -0700

    btt-preset: Refactor send-keys

--- main/dot-config/fish/functions/btt-preset.fish
+++ main/dot-config/fish/functions/btt-preset.fish
@@ -179,27 +179,31 @@ function btt-preset
         btt-preset trigger-action "$json"
     case "send-keys"
         set json (
-            printf "%s\n" $argv | jq -Rs '
+            jq -nc --arg codes "$(btt-preset get-key-codes $argv)" '
                 {
-                    "ctrl": 59, "shift": 56, "alt": 58, "cmd": 55,
-                    "a": 0, "b": 11, "c": 8, "d": 2, "e": 14, "f": 3,
-                    "g": 5, "h": 4, "i": 34, "j": 38, "k": 40, "l": 37,
-                    "m": 46, "n": 45, "o": 31, "p": 35, "q": 12, "r": 15,
-                    "s": 1, "t": 17, "u": 32, "v": 9, "w": 13, "x": 7,
-                    "y": 16, "z": 6, "space": 49, "return": 36, "left": 123,
-                    "right": 124, "up": 126, "down": 125, "fn": 63, "1": 18,
-                    "2": 19, "3": 20, "4": 21, "5": 23, "6": 22, "7": 26,
-                    "8": 28, "9": 25, "0": 29
-                } as $map
-                | rtrimstr("\n")
-                | split("\n")
-                | map(. as $key | $map[.] | if . == null then error("\($key) is not mapped") else . end)
-                | {
-                    "BTTShortcutToSend" : (. | join(","))
+                    "BTTShortcutToSend" : $codes
                 }
             '
         )
 
         btt-preset trigger-action "$json"
+    case "get-key-codes"
+        printf "%s\n" $argv | jq -Rsr '
+            {
+                "ctrl": 59, "shift": 56, "alt": 58, "cmd": 55,
+                "a": 0, "b": 11, "c": 8, "d": 2, "e": 14, "f": 3,
+                "g": 5, "h": 4, "i": 34, "j": 38, "k": 40, "l": 37,
+                "m": 46, "n": 45, "o": 31, "p": 35, "q": 12, "r": 15,
+                "s": 1, "t": 17, "u": 32, "v": 9, "w": 13, "x": 7,
+                "y": 16, "z": 6, "space": 49, "return": 36, "left": 123,
+                "right": 124, "up": 126, "down": 125, "fn": 63, "1": 18,
+                "2": 19, "3": 20, "4": 21, "5": 23, "6": 22, "7": 26,
+                "8": 28, "9": 25, "0": 29
+            } as $map
+            | rtrimstr("\n")
+            | split("\n")
+            | map(. as $key | $map[.] | if . == null then error("\($key) is not mapped") else . end)
+            | join(",")
+        '
     end
 end
