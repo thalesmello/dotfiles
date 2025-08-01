e1d150d9edc59af1971b4d6ca2c65f21d8bbb1cc
Thales Mello <thalesmello@meta.com>
2025-08-01 14:08:30 -0700

    nvim: Fix leap errors

--- main/dot-config/nvim/lua/plugins/leap.lua
+++ main/dot-config/nvim/lua/plugins/leap.lua
@@ -2,8 +2,8 @@ return {
     {
         'ggandor/leap.nvim',
         keys = {
-            {"s", "<Plug>(leap-magnetic-forward)", mode = { "n" }, desc = "Leap forward to"},
-            {"S", "<Plug>(leap-magnetic-backward)", mode = { "n" }, desc = "Leap backward to"},
+            {"s", "<Plug>(leap-forward)", mode = { "n" }, desc = "Leap forward to"},
+            {"S", "<Plug>(leap-backward)", mode = { "n" }, desc = "Leap backward to"},
             {"gs", "<Plug>(leap-from-window)", mode = { "n" }, desc = "Leap from windows"},
             {"s", "<Plug>(leap-forward-to)", mode = { "x" }, desc = "Leap operator inclusive"},
             {"gs", "<Plug>(leap-backward-to)", mode = { "x" }, desc = "Leap backwards operator inclusive"},
@@ -12,72 +12,72 @@ return {
             {"x", "<Plug>(leap-forward-till)", mode = { "o" }, desc = "Leap operator non-inclusive"},
             {"X", "<Plug>(leap-backward-till)", mode = { "o" }, desc = "Leap backwards operator inclusive"},
         },
-        config = function ()
-            local function createMagnaticJump(opts)
-                local action
-                do
-                    local first_jump = true
-                    action = function (target)
-                        local winid = target.wininfo.winid
-                        local bufid = vim.api.nvim_win_get_buf(winid)
-                        local pos = target.pos
-                        local line_text = unpack(vim.api.nvim_buf_get_lines(bufid, pos[1] - 1, pos[1], true))
-
-                        local boundaries = vim.iter(vim.fn.matchstrlist({line_text}, [[\<\w\|\w\>]])):map(function(d) return d.byteidx + 1 end):totable()
-                        local offset
-
-                        if opts.backward then
-                            if vim.list_contains(boundaries, pos[2]) then
-                                offset = 0
-                            elseif vim.list_contains(boundaries, pos[2] + 1) then
-                                offset = 1
-                            else
-                                offset = 0
-                            end
-                        else
-                            if vim.list_contains(boundaries, pos[2] + 1) then
-                                offset = 1
-                            else
-                                offset = 0
-                            end
-                        end
-
-                        local jump = require("leap.jump")
-                        jump["jump-to!"](target.pos, {
-                            winid = target.wininfo.winid,
-                            ["add-to-jumplist?"] = first_jump,
-                            mode = opts.mode,
-                            offset = offset,
-                            ["backward?"] = opts.backward,
-                            ["inclusive-op?"] = opts.inclusive,
-                        })
-                        first_jump = false
-                        return nil
-                    end
-                end
-
-                return action
-            end
-            vim.keymap.set("n", "<Plug>(leap-magnetic-forward)", function ()
-                local opts = {
-                    backward = false,
-                    inclusive = false,
-                    mode = "n",
-                }
-
-                require('leap').leap(vim.tbl_deep_extend("force", {}, opts, { action = createMagnaticJump(opts) }))
-            end, { remap = false, desc = "Leap forward to" })
-
-            vim.keymap.set("n", "<Plug>(leap-magnetic-backward)", function ()
-                local opts = {
-                    backward = true,
-                    inclusive = false,
-                    mode = "n",
-                }
-
-                require('leap').leap(vim.tbl_deep_extend("force", {}, opts, { action = createMagnaticJump(opts) }))
-            end, { remap = false, desc = "Leap forward to" })
-        end,
+        -- config = function ()
+        --     local function createMagnaticJump(opts)
+        --         local action
+        --         do
+        --             local first_jump = true
+        --             action = function (target)
+        --                 local winid = target.wininfo.winid
+        --                 local bufid = vim.api.nvim_win_get_buf(winid)
+        --                 local pos = target.pos
+        --                 local line_text = unpack(vim.api.nvim_buf_get_lines(bufid, pos[1] - 1, pos[1], true))
+        --
+        --                 local boundaries = vim.iter(vim.fn.matchstrlist({line_text}, [[\<\w\|\w\>]])):map(function(d) return d.byteidx + 1 end):totable()
+        --                 local offset
+        --
+        --                 if opts.backward then
+        --                     if vim.list_contains(boundaries, pos[2]) then
+        --                         offset = 0
+        --                     elseif vim.list_contains(boundaries, pos[2] + 1) then
+        --                         offset = 1
+        --                     else
+        --                         offset = 0
+        --                     end
+        --                 else
+        --                     if vim.list_contains(boundaries, pos[2] + 1) then
+        --                         offset = 1
+        --                     else
+        --                         offset = 0
+        --                     end
+        --                 end
+        --
+        --                 local jump = require("leap.jump")
+        --                 jump["jump-to!"](target.pos, {
+        --                     winid = target.wininfo.winid,
+        --                     ["add-to-jumplist?"] = first_jump,
+        --                     mode = opts.mode,
+        --                     offset = offset,
+        --                     ["backward?"] = opts.backward,
+        --                     ["inclusive-op?"] = opts.inclusive,
+        --                 })
+        --                 first_jump = false
+        --                 return nil
+        --             end
+        --         end
+        --
+        --         return action
+        --     end
+        --     vim.keymap.set("n", "<Plug>(leap-magnetic-forward)", function ()
+        --         local opts = {
+        --             backward = false,
+        --             inclusive = false,
+        --             mode = "n",
+        --         }
+        --
+        --         require('leap').leap(vim.tbl_deep_extend("force", {}, opts, { action = createMagnaticJump(opts) }))
+        --     end, { remap = false, desc = "Leap forward to" })
+        --
+        --     vim.keymap.set("n", "<Plug>(leap-magnetic-backward)", function ()
+        --         local opts = {
+        --             backward = true,
+        --             inclusive = false,
+        --             mode = "n",
+        --         }
+        --
+        --         require('leap').leap(vim.tbl_deep_extend("force", {}, opts, { action = createMagnaticJump(opts) }))
+        --     end, { remap = false, desc = "Leap forward to" })
+        -- end,
         extra_contexts = {"vscode", "firenvim"}
     },
     {
