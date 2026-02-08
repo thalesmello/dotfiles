8b2721a89dcd7bd5b969effe7aade689f9f47a09
Thales Mello <thalesmello@meta.com>
2026-02-08 01:11:28 -0800

    ftmini: Reindent

--- main/dot-config/nvim/lua/ftmini.lua
+++ main/dot-config/nvim/lua/ftmini.lua
@@ -12,16 +12,16 @@ function M.ftmini_config(filetype)
     local iter_filetypes = M.ft_mappings[filetype] or {filetype}
 
     local config = vim.iter(vim.list_extend({"global"}, iter_filetypes))
-    :map(function (ft)
-        local ok, ftconfig = pcall(require, "ftmini." .. ft)
-        if ok then
-            return {ftconfig}
-        else
-            return {}
-        end
-	end)
-    :flatten()
-    :fold({}, function (acc, config) return vim.tbl_deep_extend("force", acc, config) end)
+        :map(function (ft)
+            local ok, ftconfig = pcall(require, "ftmini." .. ft)
+            if ok then
+                return {ftconfig}
+            else
+                return {}
+            end
+        end)
+        :flatten()
+        :fold({}, function (acc, config) return vim.tbl_deep_extend("force", acc, config) end)
 
     return config
 end
