335fd6328dc886fe909bbf045d071f8a2be8b2f6
Thales Mello <thalesmello@meta.com>
2026-02-09 13:08:36 -0800

    neovim-ghost: Fix spawn

--- main/dot-config/fish/functions/neovim-ghost.fish
+++ main/dot-config/fish/functions/neovim-ghost.fish
@@ -9,6 +9,13 @@ function neovim-ghost
     case "focus"
         yabai-preset focus-pid "$(cat "$TMPDIR/nvim_ghost.pid")"
     case "start"
+        argparse spawn=? -- $argv
+
+        if set -q _flag_spawn
+            fish -c "neovim-ghost start" &
+            disown; and return
+        end
+
         display-message "Start GHOST"
         env NVIM_GHOST_ENABLE=1 neovide --frame buttonless --title-hidden -- --listen "$TMPDIR/nvim_ghost.socket" &
         echo "$last_pid" > "$TMPDIR/nvim_ghost.pid"
@@ -18,12 +25,13 @@ function neovim-ghost
         if neovim-ghost is-alive
             neovim-ghost focus
         else
-            neovim-ghost start
+            neovim-ghost start --spawn
         end;
     case "drop"
         set skip_focus 0
         if not neovim-ghost is-alive
-            neovim-ghost start
+            neovim-ghost start --spawn
+            sleep 2
             set skip_focus 1
         end
 
@@ -33,9 +41,11 @@ function neovim-ghost
             neovim-ghost focus
         end
     case "edit"
+        argparse minimize=? -- $argv
         set skip_focus 0
+
         if not neovim-ghost is-alive
-            neovim-ghost start
+            neovim-ghost start --spawn
             set skip_focus 1
         end
 
@@ -47,7 +57,7 @@ function neovim-ghost
 
         wait
 
-        yabai-preset minimize-pid "$(cat "$TMPDIR/nvim_ghost.pid")"
+        set -q _flag_minimize; and yabai-preset minimize-pid "$(cat "$TMPDIR/nvim_ghost.pid")"
 
     case "kill"
         kill -9 "$(cat "$TMPDIR/nvim_ghost.pid")"
