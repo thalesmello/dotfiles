1435deea4308d483525effec13d614d837330c87
Thales Mello <thalesmello@meta.com>
2026-02-12 09:36:49 -0800

    iterm: Add helper function to tell whether floating window is active

--- main/dot-config/fish/functions/iterm-preset.fish
+++ main/dot-config/fish/functions/iterm-preset.fish
@@ -5,7 +5,9 @@ function iterm-preset
     switch "$preset"
     case "new-window"
         osascript -e 'tell application "iTerm2" to create window with default profile'
+    case "is-floating-window-active"
+        string match -qr -- '<AXApplication: "iTerm2">\s*<AXWindow: "floating-terminal">' "$(btt-preset get-string-variable "focused_element_details")"
     case "*"
-        return *
+        return
     end
 end
