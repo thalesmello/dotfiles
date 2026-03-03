5fb209ed0187387b43284b2c3f3fb6daf1b1aff5
Thales Mello <thalesmello@meta.com>
2026-03-03 15:09:38 -0800

    nvim: Update clipboard

--- main/dot-config/nvim/lua/config/clipboard.lua
+++ main/dot-config/nvim/lua/config/clipboard.lua
@@ -1,25 +1,34 @@
 local should_load = require('conditional_load').should_load
 
 if should_load('ssh') then
-	local function paste()
-		local clip = vim.fn.input("Paste clipboard: ")
-		return {
-			vim.split(assert(clip), "\r"),
-			"v",
+	vim.g.clipboard = {
+		name = 'OSC 52',
+		copy = {
+			['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+			['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+		},
+		paste = {
+			['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+			['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+		},
+	}
+
+	if vim.env.TMUX ~= nil then
+		local copy = {'tmux', 'load-buffer', '-w', '-'}
+		local paste = {'bash', '-c', 'tmux refresh-client -l && sleep 0.05 && tmux save-buffer -'}
+		vim.g.clipboard = {
+			name = 'tmux',
+			copy = {
+				['+'] = copy,
+				['*'] = copy,
+			},
+			paste = {
+				['+'] = paste,
+				['*'] = paste,
+			},
+			cache_enabled = 0,
 		}
 	end
-
-	vim.g.clipboard = {
-	  name = "OSC 52",
-	  copy = {
-		["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-		["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-	  },
-	  paste = {
-		["+"] = paste,
-		["*"] = paste,
-	  },
-	}
 else
 	vim.g.clipboard = {
 		name = 'mac-os like',
