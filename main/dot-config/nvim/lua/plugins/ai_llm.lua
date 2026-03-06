0fb51a883ebe6c4e21d0ad65edc3e17a3adf1c8c
Thales Mello <thalesmello@meta.com>
2026-03-05 17:22:58 -0800

    nvim: Make claudecode work with dirvish

--- main/dot-config/nvim/lua/plugins/ai_llm.lua
+++ main/dot-config/nvim/lua/plugins/ai_llm.lua
@@ -1,47 +1,5 @@
 return {
    {
-      "olimorris/codecompanion.nvim",
-      opts = {
-         interactions = {
-            chat = {
-               adapter = "gemini",
-            },
-            inline = {
-               adapter = "gemini",
-            },
-            cmd = {
-               adapter = "gemini",
-            }
-         },
-         adapters = {
-            gemini = function()
-               return require("codecompanion.adapters").extend("gemini", {
-                  env = {
-                     api_key = 'cmd:op read "op://Personal/thales-workflow Gemini API Key/credential" --no-newline',
-                  },
-               })
-            end,
-         },
-      },
-      dependencies = {
-         "nvim-lua/plenary.nvim",
-         "nvim-treesitter/nvim-treesitter",
-         "j-hui/fidget.nvim",
-      },
-      init = function ()
-         require('config.code_companion_fidget_spinner'):init()
-      end,
-      config = function (_, opts)
-         require("codecompanion").setup(opts)
-
-         vim.keymap.set({ "n", "v" }, "<leader>ca", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
-         vim.keymap.set({ "n", "v" }, "<leader>cc", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
-         vim.keymap.set("v", "<leader>ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
-
-         vim.cmd.cabbrev("cc CodeCompanion")
-      end
-   },
-   {
       "coder/claudecode.nvim",
       dependencies = { "folke/snacks.nvim" },
       opts = function()
@@ -86,6 +44,18 @@ return {
             "<cmd>ClaudeCodeTreeAdd<cr>",
             desc = "Add file",
             ft = { "NvimTree", "neo-tree", "oil", "minifiles", "netrw" },
+         },
+         {
+            "<leader>as",
+            "<cmd>argdo ClaudeCodeAdd %<cr>",
+            desc = "Add file",
+            ft = {"dirvish"},
+         },
+         {
+            "<leader>.",
+            "<cmd>ClaudeCodeAdd <cWORD><cr>",
+            desc = "Add file",
+            ft = {"dirvish"},
          },
          -- Diff management
          { "<leader>aa", "<cmd>ClaudeCodeDiffAccept<cr>", desc = "Accept diff" },
