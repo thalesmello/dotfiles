b9e2c02038a884ee62f387dcdca741902c0347cf
Thales Mello <thalesmello@meta.com>
2026-03-09 23:56:02 -0700

    hs: Add dark mode

--- main/dot-hammerspoon/init.lua
+++ main/dot-hammerspoon/init.lua
@@ -5,6 +5,8 @@ local util = require('util')
 local hyperShift = {"ctrl", "cmd", "alt", "shift"}
 local hyper = {"ctrl", "cmd", "shift"}
 
+hs.console.darkMode(true)
+
 hs.hotkey.bind(hyperShift, 'B', function() hs.toggleConsole() end)
 
 if not hs.ipc.cliStatus(nil, true) then
