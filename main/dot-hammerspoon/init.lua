9b91b092bb898fabcd7d682424d5b2ceebcb2445
Thales Mello <thalesmello@meta.com>
2026-03-10 12:59:40 -0700

    hs: add hammerspoon file watch for reloading config

--- main/dot-hammerspoon/init.lua
+++ main/dot-hammerspoon/init.lua
@@ -16,5 +16,22 @@ end
 require("keybindings")
 require("audiodevice")
 
+-- Auto-reload config when files change
+local watchPaths = {
+  os.getenv("HOME") .. "/.hammerspoon",
+  os.getenv("HOME") .. "/.local_dotfiles/local_hammerspoon",
+}
+
+local reloadWatchers = {}
+for _, path in ipairs(watchPaths) do
+  local watcher = hs.pathwatcher.new(path, function(files)
+    hs.reload()
+  end)
+  if watcher then
+    watcher:start()
+    table.insert(reloadWatchers, watcher)
+  end
+end
+
 util.notify("Hammerspoon!")
 
