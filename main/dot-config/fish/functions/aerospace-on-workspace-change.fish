3f1acbbae8d01dcf9ee76839c32ae534b1accbe3
Thales Mello <thalesmello@meta.com>
2026-06-10 12:58:11 -0700

    aerospace: Fix stdin bug and cleanup on workspace change

--- main/dot-config/fish/functions/aerospace-on-workspace-change.fish
+++
@@ -1,9 +0,0 @@
-function aerospace-on-workspace-change
-    # Currently too slow. I'm disabling it to check whether it improves performance
-    # aerospace-auto-summon \
-    #     --when-workspace "?"
-    aerospace-sticky-windows \
-        --except-workspace "?" \
-        -w "Picture in Picture" \
-        -w ".*about:blank - Thales \(Personal\).*"
-end
