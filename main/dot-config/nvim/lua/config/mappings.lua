2f70983825bedd215e43cfd1805adda9c5856eb6
Thales Mello <thalesmello@meta.com>
2026-07-20 13:08:33 -0700

    nvim: Remove unused cmap

--- main/dot-config/nvim/lua/config/mappings.lua
+++ main/dot-config/nvim/lua/config/mappings.lua
@@ -25,8 +25,6 @@ vim.keymap.set({"n", "x", "o"}, "H", "^", { remap = false })
 vim.keymap.set({"n", "x", "o"}, "L", "$", { remap = false })
 vim.keymap.set({"n", "x", "o"}, "<leader>me", "<cmd>messages<cr>", { remap = false })
 vim.keymap.set({"n", "v"}, "z-", "zMzv", { remap = false })
--- I frequently mistype w' intending to type "w<cr>". This should prevent accidentally creating a w' file
-vim.keymap.set({"c"}, "w'", "w", { remap = false })
 
 local lineContinuationToggle = [=[:luado if line:match('\\?%s*#.*$') ~= nil then return line:gsub('^(%s*)\\?.-# ', '%1') elseif vim.fn.getline('.'):match('\\$') then return line:gsub('^(%s*)(%S-.-)$', '%1\\ # %2') else return line:gsub('^(%s*)(%S-.-)$', '%1# %2') end<cr>]=]
 
