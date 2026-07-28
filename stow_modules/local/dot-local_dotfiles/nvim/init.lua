7ef3a24031f9d56d3d7def1ed33a066b4d162847
Thales Mello <thalesmello@meta.com>
2026-07-28 16:40:17 -0700

    stow: Fix references to old main directory

--- stow_modules/local/dot-local_dotfiles/nvim/init.lua
+++ stow_modules/local/dot-local_dotfiles/nvim/init.lua
@@ -3,34 +3,34 @@ vim.g.projectionist_heuristics = vim.tbl_deep_extend(
     vim.g.projectionist_heuristics or {},
     {
         ["src/dotfiles/"] = {
-            ["src/dotfiles/main/dot-skhdrc_main"] = {
-                alternate= { "src/dotfiles/local/dot-skhdrc_local", "src/dotfiles/main/dot-skhdrc" },
+            ["src/dotfiles/stow_modules/main/dot-skhdrc_main"] = {
+                alternate= { "src/dotfiles/local/dot-skhdrc_local", "src/dotfiles/stow_modules/main/dot-skhdrc" },
                 type= "skhd_main",
             },
             ["src/dotfiles/local/dot-skhdrc_local"] = {
-                alternate = { "src/dotfiles/main/dot-skhdrc", "src/dotfiles/main/dot-skhdrc_main" },
+                alternate = { "src/dotfiles/stow_modules/main/dot-skhdrc", "src/dotfiles/stow_modules/main/dot-skhdrc_main" },
                 type = "skhd_local",
             },
-            ["src/dotfiles/main/dot-skhdrc"] = {
-                alternate = { "src/dotfiles/main/dot-skhdrc_main", "src/dotfiles/local/dot-skhdrc_local" },
+            ["src/dotfiles/stow_modules/main/dot-skhdrc"] = {
+                alternate = { "src/dotfiles/stow_modules/main/dot-skhdrc_main", "src/dotfiles/local/dot-skhdrc_local" },
                 type = "skhd",
             },
-            ["src/dotfiles/main/sketchybarrc"] = {
+            ["src/dotfiles/stow_modules/main/sketchybarrc"] = {
                 alternate="src/dotfiles/local/dot-sketchybarrc_local",
                 type= "sketchybarrc",
                 dispatch= "sketchybar --reload",
             },
             ["src/dotfiles/local/dot-sketchybarrc_local"] = {
-                alternate = "src/dotfiles/main/sketchybarrc",
+                alternate = "src/dotfiles/stow_modules/main/sketchybarrc",
                 type = "sketchybarrc_local",
                 dispatch= "sketchybar --reload",
             },
-            ["src/dotfiles/main/nvim/init.lua"] = {
+            ["src/dotfiles/stow_modules/main/nvim/init.lua"] = {
                 alternate="src/dotfiles/local/dot-local_dotfiles/nvim/init.lua",
                 type= "nviminit",
             },
             ["src/dotfiles/local/dot-local_dotfiles/nvim/init.lua"] = {
-                alternate = "src/dotfiles/main/nvim/init.lua",
+                alternate = "src/dotfiles/stow_modules/main/nvim/init.lua",
                 type = "localnvim",
             },
         },
